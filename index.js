ffb9fcaa6e0177aefcd23f6729e06f5438513991
LGustavoDev <miguelegu@hotmail.com>
2023-12-07 21:01:40 -0300

    Inicio do Projeto

---
+++ index.js
@@ -0,0 +1,38 @@
+const readline = require('readline');
+
+// Função para calcular o nível do jogador
+function calcularNivel(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    let nivel = '';
+
+    if (vitorias < 10) {
+        nivel = 'Ferro';
+    } else if (vitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (vitorias <= 50) {
+        nivel = 'Prata';
+    } else if (vitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (vitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (vitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    return `O Herói tem saldo de ${saldo} e está no nível de ${nivel}`;
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Digite a quantidade de vitórias: ', (vitorias) => {
+    rl.question('Digite a quantidade de derrotas: ', (derrotas) => {
+        const resultado = calcularNivel(parseInt(vitorias), parseInt(derrotas));
+        console.log(resultado);
+        rl.close();
+    });
+});
